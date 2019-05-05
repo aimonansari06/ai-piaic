75ad65017e5de99f725d9bca833c8eb7ea50b0cb
aimonansari06 <aimon_ansari06@outlook.com>
2019-05-05 19:31:27 +0500

    ai-practice

---
+++ aipractice1.py
@@ -0,0 +1,17 @@
+a=int(input("Enter 1st number "))
+b=int(input("Enter 2nd number "))
+o=input("Enter Operator ")
+if o=="+":
+    x = a + b
+    print( x )
+if o=="-":
+    x = a - b
+    print( x )
+if o=="*":
+    x = a * b
+    print( x )
+if o=="/":
+    x = a / b
+    print( x )
+print("Nachooo -_-")
+    
